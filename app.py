96761e7882f9b1c2632ee48d449eb13afbc58225
Diego Fernando Marin <dfmarin@gmail.com>
2025-05-13 19:18:15 -0500

    se agrega la aplicación principal

---
+++ app.py
@@ -0,0 +1,19 @@
+"""
+Script principal para ejecutar la aplicación Flask.
+"""
+import os
+from musica_api import create_app
+from dotenv import load_dotenv
+
+# TODO: Cargar variables de entorno desde archivo .env si existe
+
+# TODO: crear la aplicación
+
+if __name__ == "__main__":
+    # TODO: Obtener puerto del ambiente o usar 5000 por defecto
+    
+    # TODO: Determinar si se debe usar modo debug
+    
+    # TODO: Ejecutar aplicación
+
+    pass
